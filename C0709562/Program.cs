75b15555144e9250d9459a6f1e85e191d38894aa
Sukhraj singh C0709562 <46693375+sukh2231@users.noreply.github.com>
2019-04-18 14:24:56 -0400

    Add files via upload
    Start up Code

--- C0709562/Program.cs
+++ C0709562/Program.cs
@@ -3,13 +3,48 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using System.Collections;
+using System.IO;
 namespace C0709562
 {
     class Program
     {
+        ArrayList Beowulf;
         static void Main(string[] args)
         {
+            Program p = new Program();
+            p.Beowulf = new ArrayList();
         }
+        public void Run() { this.ReadTextFiles(); }
+        public void ReadTextFiles()
+        {
+            using (StreamReader file = new StreamReader("c:/area51/beowulf.txt"))
+            {
+                int counter = 0;
+                string ln;
+                while ((ln = file.ReadLine()) != null)
+
+                {
+                    Console.WriteLine(ln);
+                    Beowulf.Add(ln);
+                }
+                file.Close();
+                Console.WriteLine($"File has {counter} lines.");
+
+            }
+        }
+        public int FindNumberOfBlankSapces(string line)
+        {
+            int countletters = 0;
+            int countSpaces = 0;
+            foreach (char c in line)
+            {
+                if (char.IsLetter(c)) { countletters++; }
+                if (char.IsWhiteSpace(c)) { countSpaces++; }
+            }
+            return countSpaces;
+        }
+
     }
 }
+
